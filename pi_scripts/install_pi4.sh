0e4a75fdec903a5ce3f7f8d7fe8c607c008303e7
zarbokk <timhansen93@googlemail.com>
2020-01-27 10:23:17 +0100

    update with cnmake install

--- pi_scripts/install_pi4.sh
+++ pi_scripts/install_pi4.sh
@@ -1,6 +1,5 @@
 echo "PS1='\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\] '">> ~/.bashrc
 cd ~/Documents
-sudo apt purge cmake
 git clone https://github.com/Kitware/CMake.git
 cd CMake
 ./bootstrap && make && sudo make install
