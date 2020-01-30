40183f2fc055d491a415da064a862abb3f744a4f
zarbokk <timhansen93@googlemail.com>
2020-01-30 13:23:54 +0100

    update with cnmake install

--- opencv4_1_1_install/pi_install.sh
+++ opencv4_1_1_install/pi_install.sh
@@ -2,7 +2,6 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 python3-dev -y
 
-
 cd ~
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.1.1.zip
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.1.1.zip
@@ -10,8 +9,8 @@ unzip opencv.zip
 unzip opencv_contrib.zip
 mv opencv-4.1.1 opencv
 mv opencv_contrib-4.1.1 opencv_contrib
-
-
+sudo rm opencv.zip
+sudo rm opencv_contrib.zip
 cd ~/opencv
 mkdir build
 cd build
---
+++ pi_scripts/localization_install.sh
@@ -0,0 +1,6 @@
+cd ~/catkin_ws/src
+
+git clone https://github.com/DanielDuecker/muAUV-localization_ros.git
+cd ..
+
+
