4fc669f0026ec984e5d6d9a00f19883e2483b24f
Your Name <you@example.com>
2020-01-30 08:34:07 +0000

    dsaudasnbdjas

--- opencv4_1_1_install/install_opencv4.sh
+++ opencv4_1_1_install/install_opencv4.sh
@@ -19,16 +19,14 @@ mkdir build
 cd build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-  -DENABLE_PRECOMPILED_HEADERS=OFF \
+       -D ENABLE_PRECOMPILED_HEADERS=OFF \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D INSTALL_PYTHON_EXAMPLES=ON \
 	-D INSTALL_C_EXAMPLES=OFF \
 	-D OPENCV_ENABLE_NONFREE=ON \
 	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
 	-D PYTHON_EXECUTABLE=/usr/bin/python \
-	-D BUILD_EXAMPLES=ON ..
+	-D BUILD_EXAMPLES=ON .. 
 make -j4
-
-
 sudo make install
 sudo ldconfig
