c8f2ae66a6f53c19b5ac7583b87d4f5adffb1089
zarbokk <timhansen93@googlemail.com>
2020-01-31 09:17:57 +0100

    update with cnmake install

--- pi_scripts/localization_install.sh
+++ pi_scripts/localization_install.sh
@@ -1,6 +1,8 @@
 cd ~/catkin_ws/src
-
+git clone https://github.com/AprilRobotics/apriltag_ros.git
+git clone https://github.com/AprilRobotics/apriltag.git
 git clone https://github.com/DanielDuecker/muAUV-localization_ros.git
 cd ..
+catkin build
 
 
