564850f64ef1b4d79181f8d48c0f75c4c6d8c8d9
zarbokk <timhansen93@googlemail.com>
2020-01-31 11:34:14 +0100

    update with cnmake install

--- pi_scripts/localization_install.sh
+++ pi_scripts/localization_install.sh
@@ -1,8 +1,11 @@
 cd ~/catkin_ws/src
-git clone https://github.com/AprilRobotics/apriltag_ros.git
-git clone https://github.com/AprilRobotics/apriltag.git
 git clone https://github.com/DanielDuecker/muAUV-localization_ros.git
 cd ..
+
+rosinstall_generator apriltag_ros --rosdistro melodic --deps --wet-only --tar > melodic-localization.rosinstall
+wstool merge -t src melodic-localization.rosinstall
+wstool update -t src
+
 catkin build
 
 
