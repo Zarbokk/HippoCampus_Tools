638a0b2ab45f76db9e2223b171d992b622050bae
zarbokk <timhansen93@googlemail.com>
2020-02-11 12:45:33 +0100

    added usb_cam

--- hippocampus_tools_ros/scripts/mavros_local_pos_ENU_to_NED.py
+++ hippocampus_tools_ros/scripts/mavros_local_pos_ENU_to_NED.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 publisher_position_boat_NED = rospy.Publisher("/mavros/local_position/pose_NED", PoseStamped, queue_size=1)
 
 rate = None
+rviz=True
+if rviz:
+    publisher_marker = rospy.Publisher('/pose_boat_NED', Marker, queue_size=1)
 
 qx_180 = Quaternion(axis=[1, 0, 0], angle=np.pi)
 qz_90p = Quaternion(axis=[0, 0, 1], angle=np.pi / 2)
@@ -49,10 +52,33 @@ def callback(msg):
     publisher_position_boat_NED.publish(NED)
 
 
+    if rviz:
+        marker = Marker()
+        marker.header.frame_id = "global_tank"
+        marker.id = 0
+        marker.type = marker.MESH_RESOURCE
+        marker.action = marker.ADD
+        marker.scale.x = 20
+        marker.scale.y = 20
+        marker.scale.z = 20
+        marker.color.r = 1
+        marker.color.a = 1  # transparency
+        marker.pose.orientation.w = tmpquat.w
+        marker.pose.orientation.x = tmpquat.x
+        marker.pose.orientation.y = tmpquat.y
+        marker.pose.orientation.z = tmpquat.z
+        marker.pose.position.x = pos_boat_x  # x
+        marker.pose.position.y = pos_boat_y  # y
+        marker.pose.position.z = pos_boat_z  # z
+        marker.mesh_resource = "package://hippocampus_tools_ros/models/uuv_hippocampus.stl"
+        publisher_marker.publish(marker)
+
+
 def main():
     rospy.init_node('enu_to_ned')
     global rate
     rate = rospy.Rate(30)
+
     rospy.Subscriber("/mavros/local_position/pose", PoseStamped, callback, queue_size=1)
 
     while not rospy.is_shutdown():
